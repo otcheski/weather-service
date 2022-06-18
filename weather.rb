4fb6e2f65275d2770e7e9930701525f74c9ea498
otcheski <a.otcheski@gmail.com>
2022-06-18 19:47:35 +0700

    1.1 version - добавлен список городов. Для выбора города используется название, а не его код.

--- weather.rb
+++ weather.rb
@@ -1,32 +1,34 @@
 require 'net/http'
-require 'cgi'
 require 'rexml/document'
+require 'json'
 
 require_relative 'lib/meteoservice_forecast'
 
-CITIES = {
-  37 => 'Москва',
-  69 => 'Санкт-Петербург',
-  99 => 'Новосибирск',
-  59 => 'Пермь',
-  115 => 'Орел',
-  141 => 'Братск',
-  199 => 'Краснодар',
-  175 => 'Томск'
-}.invert.freeze
+current_path = File.dirname(__FILE__)
+cities_file_name = "#{current_path}/data/cities.json"
 
-city_names = CITIES.keys
-
-puts 'Погоду какого города вы хотите узнать?'
-city_names.each_with_index { |name, index| puts "#{index + 1}: #{name}"}
-city_index = gets.to_i
-
-unless city_index.between?(1, city_names.size)
-  puts "Введите число от 1 до #{city_names.size}"
-  city_index = gets.to_i
+begin
+  file = File.read(cities_file_name)
+rescue Errno::ENOENT
+  abort 'File not found'
 end
 
-city_id = CITIES[city_names[city_index - 1]]
+cities_names = JSON.parse(file)
+
+puts 'Погоду в каком городе России вы хотите узнать?'
+user_input = STDIN.gets.chomp
+
+city_id = nil
+city_name = nil
+
+cities_names.each do |key, value|
+  if value == user_input.capitalize
+    city_id = key
+    city_name = value.to_s.capitalize
+  end
+end
+
+abort "В нашем списке нет города #{user_input}" if city_id.nil?
 
 URL = "https://xml.meteoservice.ru/export/gismeteo/point/#{city_id}.xml".freeze
 
@@ -36,10 +38,9 @@ doc = REXML::Document.new(response.body)
 
 forecast_nodes = doc.root.elements['REPORT/TOWN'].elements.to_a
 
-city_name = CGI.unescape(doc.root.elements['REPORT/TOWN'].attributes['sname'])
-
+puts
 puts '--------------------------'
-puts city_name
+puts "Погода в городе #{city_name}"
 puts '--------------------------'
 
 forecast_nodes.each do |node|
